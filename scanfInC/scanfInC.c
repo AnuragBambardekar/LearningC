0b4b51bc951bf9505a759fec480819dc65984bbf
AnuragBambardekar <anurag.bambardekar@gmail.com>
2023-01-20 21:02:11 -0500

    scanf in C to take inputs from I/O - keyboard

---
+++ scanfInC/scanfInC.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    printf("Enter first number: \n");
+    scanf("%d", &a);
+    printf("Enter second number: \n");
+    scanf("%d", &b);
+    printf("%d + %d = %d \n", a, b, a+b);
+    return 0;
+}
